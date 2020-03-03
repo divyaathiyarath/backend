360caac29a5cf144734060ea6445b3b829aceded
vigin <vpvigin@gmail.com>
2020-03-03 22:44:36 +0530

    mongo connection added

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require ("mongoose");
+require('dotenv').config()
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +17,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+
+// mongodb connection
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true },function (err, res) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log(`Mongoose connected on ${process.env.MONGODB_URI}`)
+  }
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
